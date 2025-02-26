888db29eb1070187a07b5702d4bbb334c2bdeda2
Sidhant492 <sidhantkumar492@gmail.com>
2025-02-26 13:55:04 +0530

    mega blog app

---
+++ src/components/AuthLayout.jsx
@@ -0,0 +1,35 @@
+import React, {useEffect, useState} from 'react'
+import {useSelector} from 'react-redux'
+import {useNavigate} from 'react-router-dom'
+
+export default function Protected({children, authentication = true}) {
+
+    const navigate = useNavigate()
+    const [loader, setLoader] = useState(true)
+    const authStatus = useSelector(state => state.auth.status)
+    console.log("autStauts:",authStatus);
+    console.log("authentication",authentication);
+
+    useEffect(() => {
+        //TODO: make it more easy to understand
+
+        // if (authStatus ===true){
+        //     navigate("/")
+        // } else if (authStatus === false) {
+        //     navigate("/login")
+        // }
+        
+        //let authValue = authStatus === true ? true : false
+
+        if(authentication && authStatus !== authentication){
+            console.log("111111111111111111111111");
+            navigate("/login")
+        } else if(!authentication && authStatus !== authentication){
+            console.log("2222222222222");
+            navigate("/")
+        }
+        setLoader(false)
+    }, [authStatus, navigate, authentication])
+
+  return loader ? <h1>Loading...</h1> : <>{children}</>
+}
